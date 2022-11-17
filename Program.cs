0fd09ebf5bf795a0b4e7ecd1533d48622e0c22dc
Eszter Digruber <digruber.eszter@verebelyszki.hu>
2022-11-17 14:11:23 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -2,9 +2,29 @@
 {
     class Program
     {
+        static int[] DolgozokTMB;
+        static double[] UtHosszTMB;
+        static Random rnd = new Random();
+
         static void Main(string[] args)
         {
             Beugro1();
+            Utepitok();
+        }
+
+        private static void Utepitok()
+        {
+            Feladat1();
+        }
+
+        private static void Feladat1()
+        {
+            Console.WriteLine("Feladat 1:");
+            for (int i = 0; i < 18; i++)
+            {
+                DolgozokTMB[i] = rnd.Next(8, 13);
+                UtHosszTMB[i] = rnd.Next(5055, 7555) / 100.00;
+            }
         }
 
         private static void Beugro1()
