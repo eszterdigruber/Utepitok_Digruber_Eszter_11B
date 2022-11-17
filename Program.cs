1f3726bec683fd138babda2a1331a1eb61f115e0
Eszter Digruber <digruber.eszter@verebelyszki.hu>
2022-11-17 14:19:57 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -2,24 +2,34 @@
 {
     class Program
     {
-        static int[] DolgozokTMB;
-        static double[] UtHosszTMB;
+        static int[] DolgozokTMB = new int[18];
+        static double[] UtHosszTMB = new double[18];
         static Random rnd = new Random();
 
         static void Main(string[] args)
         {
-            Beugro1();
+            //Beugro1();
             Utepitok();
         }
 
         private static void Utepitok()
         {
             Feladat1();
+            Feladat_02();
+        }
+
+        private static void Feladat_02()
+        {
+            Console.WriteLine("Feladat 2:");
+            for (int i = 0; i < 18; i++)
+            {
+                Console.WriteLine($"{i + 1}. nap -> dolgozók szánma: {DolgozokTMB[i]} -> {UtHosszTMB[i]:0.00}");
+            }
         }
 
         private static void Feladat1()
         {
-            Console.WriteLine("Feladat 1:");
+            //Console.WriteLine("Feladat 1:");
             for (int i = 0; i < 18; i++)
             {
                 DolgozokTMB[i] = rnd.Next(8, 13);
@@ -27,7 +37,7 @@
             }
         }
 
-        private static void Beugro1()
+        /*private static void Beugro1()
         {
             int[] FeltoltTMB = { 23, 52, 62, 42, 73, 45, 32, 25, 36, 58, 39, 42, 12, 40 };
             int CsereSzam;
@@ -45,6 +55,6 @@
             }
             for (int i = 0; i < FeltoltTMB.Length; i++)
             { Console.Write($"{FeltoltTMB[i]}, ");  }
-        }
+        }*/
     }
 }
